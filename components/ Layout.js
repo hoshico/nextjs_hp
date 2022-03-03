05f202737f67736930323598d7074eaa6891d953
hoshico <melancholinista.yasu@gmail.com>
2022-03-04 08:11:26 +0900

    update Layout.js

--- components/ Layout.js
+++ components/ Layout.js
@@ -33,5 +33,16 @@ export default function Layout({ children, title = "HP by Nextjs" }){return (
     <main className="flex flex-1 justify-center items-center flex-col w-screen">
       {children}
     </main>
+    <footer className="w-full h-12 justify-center items-center border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
   </div>
 )}
